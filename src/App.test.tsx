f08f93f65893d8e2216c9892be26f76d4f44b499
Benoit Patra <benoit.patra@gmail.com>
2021-04-24 15:42:17 +0200

    Discard test for React

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import AddinApp from './AddinApp';
-
-test('renders learn react link', () => {
-  render(<AddinApp />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
